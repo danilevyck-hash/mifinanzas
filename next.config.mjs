16c126e517f2bce025910749a27f879d68f0f6a2
danilevyck-hash <danilevyck@gmail.com>
2026-04-10 19:26:11 -0500

    feat: add Sentry error monitoring

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  org: "fashion-group",
+  project: "mifinanzas",
+  widenClientFileUpload: true,
+  disableLogger: true,
+});
